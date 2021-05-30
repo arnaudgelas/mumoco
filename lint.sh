241e8232941603838eb431a9029b9f6c07f0c381
Arnaud Gelas <arnaudgelas@gmail.com>
2021-05-30 21:19:16 +0200

    use MAX_LINE_LENGTH in lint.sh

--- lint.sh
+++ lint.sh
@@ -3,14 +3,16 @@
 set -x
 set -e
 
+MAX_LINE_LENGTH=120
+
 echo "Check format with black"
-poetry run black -l 120 --check .
+poetry run black -l $MAX_LINE_LENGTH --check .
 
 poetry run isort --profile black --check .
 
 echo "Lint with flake8"
 poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv
-poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --extend-exclude=.venv
+poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=$MAX_LINE_LENGTH --statistics --extend-exclude=.venv
 
 echo "Run bandit"
 poetry run bandit -r src/ --exclude .venv
