703f84b927007632feac6dabb8031841a694f0f5
Arnaud Gelas <arnaudgelas@gmail.com>
2021-05-31 20:28:54 +0200

    Run mypy in lint.sh

--- lint.sh
+++ lint.sh
@@ -8,11 +8,15 @@ MAX_LINE_LENGTH=120
 echo "Check format with black"
 poetry run black -l $MAX_LINE_LENGTH --check .
 
+echo "Check imports with isort"
 poetry run isort --profile black --check .
 
 echo "Lint with flake8"
 poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv
 poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=$MAX_LINE_LENGTH --statistics --extend-exclude=.venv
 
-echo "Run bandit"
+echo "Security analysis with bandit"
 poetry run bandit -r src/ --exclude .venv
+
+echo "Type checking with mypy"
+poetry run mypy
