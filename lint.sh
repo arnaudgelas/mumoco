e5f686f155d7a1ecae012aede6e2af1279195e1e
Arnaud Gelas <arnaudgelas@gmail.com>
2021-05-31 20:46:35 +0200

    Set-up pylint

--- lint.sh
+++ lint.sh
@@ -15,6 +15,9 @@ echo "Lint with flake8"
 poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv
 poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=$MAX_LINE_LENGTH --statistics --extend-exclude=.venv
 
+echo "Lint with pylint"
+poetry run pylint --disable=C0114,C0115,C0116 --max-line-length=120 src/
+
 echo "Security analysis with bandit"
 poetry run bandit -r src/ --exclude .venv
 
